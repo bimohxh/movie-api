5486b98eb019b96b9002fe877bd00b593533bc6a
hxh <1246996371@qq.com>
2017-03-12 14:56:16 +0800

    'readytoupload'

---
+++ routes/qiniu.js
@@ -0,0 +1,13 @@
+'use strict'
+var router = require('express').Router()
+var url = require('url');
+
+var qiniu = require('../lib/qiniu')
+
+
+
+router.get('/token', function (req, res, next) {
+
+})
+
+module.exports = router;
