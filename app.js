ed824cb487b9d8751148744461ebfb07047d695b
hxh <1246996371@qq.com>
2017-03-13 17:52:02 +0800

    add cross origin

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var AV = require('leanengine');
+var cors = require('cors')
 
 // 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
 require('./cloud');
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+app.use(cors())
 
 // 设置默认超时时间
 app.use(timeout('15s'));
